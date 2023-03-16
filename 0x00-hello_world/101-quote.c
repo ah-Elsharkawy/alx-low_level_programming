5d568644692c5b676c36b814c584674486b92f80
AhmedElsharkawy <aelsharkawy56@gmail.com>
2023-03-16 10:45:47 +0200

    optional

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return 1;
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
