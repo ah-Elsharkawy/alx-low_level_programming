a250d494bb18442ec8dcdf7b53eec77d5ec9c057
AhmedElsharkawy <aelsharkawy56@gmail.com>
2023-03-29 22:31:21 +0200

    optional

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,28 +7,19 @@
  */
 char *rot13(char *s)
 {
-	int i;
-	int j;
-	
+int i;
+int j;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-        if (s[i] >= 65 && s[i] <= 77 || s[i] >= 97 && s[i] <= 109)
-        {
-            s[i] += 13;
-        }
-        else
-        {
-            s[i] -= 13;
-        }
-        
-	}
-	return (s);
-}
-
-int main()
+for (i = 0; s[i] != '\0'; i++)
 {
-    char str[] = "abcdefghijklmnopqrstuvwxyz";
-    rot13(str);
-    printf("%s\n", str);
+if (s[i] >= 65 && s[i] <= 77 || s[i] >= 97 && s[i] <= 109)
+{
+s[i] += 13;
+}
+else
+{
+s[i] -= 13;
+}
+}
+return (s);
 }
