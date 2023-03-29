2396032713a019a2a0d98e40682830bb5c3b1701
AhmedElsharkawy <aelsharkawy56@gmail.com>
2023-03-29 22:34:11 +0200

    optional

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * rot13 - encrypts code
+ * rot13 - encrypts code the dumb way
+ * the previous commit was better and easier to understand
+ * but clearly they want this solution
  * @s: string to encrypt
- * Return: the encrypted string
+ * Return: char value
  */
 char *rot13(char *s)
 {
-int i;
-int j;
+	int i;
+	int j;
+	char encode1[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char encode2[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
 
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] >= 65 && s[i] <= 77 || s[i] >= 97 && s[i] <= 109)
-{
-s[i] += 13;
-}
-else
-{
-s[i] -= 13;
-}
-}
-return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; encode1[j]; j++)
+		{
+			if (s[i] == encode1[j])
+			{
+				s[i] = encode2[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
