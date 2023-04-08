18581bf8d47de6d34d12e202aa9d4a6f44c02467
AhmedElsharkawy <aelsharkawy56@gmail.com>
2023-04-08 05:11:09 +0200

    mandatory

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,11 @@
  * @argc: The number of arguments passed to the program at compilation.
  * @argv: An array of pointers to the arguments.
  *
+ * Return: 0;
  */
 
-void main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 printf("%s\n", argv[0]);
+return (0);
 }
