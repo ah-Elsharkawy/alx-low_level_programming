bb4757d03ce0acba800a19e54ae45d029962ac3e
AhmedElsharkawy <aelsharkawy56@gmail.com>
2023-04-10 07:25:56 +0200

    ,andatory

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - print a char
+ * @c: a char
+ * Description: function that prins a char
+ * Return: 1 or 0.
+ */
+
+int _putchar(char c)
+{
+    putchar(c);
+	return (0);
+}
